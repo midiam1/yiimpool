ba47ef87cd2d7fb4df0bafcd441b8b8e026479a3
midiam1 <admhimmeros@gmail.com>
2021-12-17 23:16:53 -0400

    Update piscina.sh

--- piscina.sh
+++ piscina.sh
@@ -8,5 +8,21 @@
 # Tomado de https://github.com/xavatar/yiimp_install_scrypt
 
 # Actualizo el sistema
-    sudo apt update -y && upgrade -y 
-    
+    sudo apt update -y && upgrade -y && clear 
+
+# Creo al usuario que va a manejar la piscina
+    sudo adduser teseo
+    sudo adduser teseo sudo
+
+# Cambio al usuario de la piscina
+    su - teseo
+
+# Instalo GIT
+    sudo apt install git -y && clear
+
+# Descargo la piscina
+    git clone https://github.com/xavatar/yiimp_install_scrypt.git
+    cd yiimp_install_scrypt/
+    bash install.sh
+
+
