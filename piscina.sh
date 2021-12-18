264ca34546cc7f6877b29254f8e19c823a74f5fb
midiam1 <admhimmeros@gmail.com>
2021-12-17 23:11:33 -0400

    Create piscina.sh

---
+++ piscina.sh
@@ -0,0 +1,12 @@
+# Piscina de minado con base en YIIMP
+# Proyectos Himmeros
+# 122021 - v0.1
+
+# Limpio la pantalla
+    clear
+
+# Tomado de https://github.com/xavatar/yiimp_install_scrypt
+
+# Actualizo el sistema
+    sudo apt update -y && upgrade -y 
+    
